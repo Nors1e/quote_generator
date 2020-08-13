89dfdab4765c5323f13f19c774a2a670e84832dc
Caleb Stout <hisurfingc@yahoo.com>
2020-08-12 23:45:49 -0700

    Update of Async function

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -1,12 +1,20 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 // Get Quote From API
 async function getQuote() {
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-        console.log(data);
+        // const response = await fetch(proxyUrl + apiUrl);
+        // const data = await response.json();
+        // authorText.innerText = data.quoteAuthor;
+        // quoteText.innerText = data.quoteText;
     } catch (error) {
-        console.log('whoops, no quote', error);
+        getQuote();
     }
 }
 
